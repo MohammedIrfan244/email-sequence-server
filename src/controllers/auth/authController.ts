7b6b56bda2b9ae0ad902e762e554b81de236c354
MohammedIrfan244 <kuttappuirfan244@gmail.com>
2025-05-06 13:28:11 +0530

    refactor: update authController to include NextFunction in request handlers

--- src/controllers/auth/authController.ts
+++ src/controllers/auth/authController.ts
@@ -1,15 +1,14 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import User from "../../models/userModel";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import CustomError from "../../lib/utils/CustomError";
 
-const registerUser = async (req: Request, res: Response) => {
+const registerUser = async (req: Request, res: Response,next : NextFunction) => {
   const { username, email, password } = req.body;
   const existingUser = await User.findOne({ email });
   if (existingUser) {
-    console.log("test");
-    return new CustomError("User already exists", 400);
+    return next(new CustomError("User already exists", 400))
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({
@@ -18,7 +17,7 @@ const registerUser = async (req: Request, res: Response) => {
     password: hashedPassword,
   });
   if (!newUser) {
-    return new CustomError("User registration failed", 400);
+    return next(new CustomError("User registration failed", 500))
   }
   res.status(201).json({
     status: "success",
@@ -26,15 +25,15 @@ const registerUser = async (req: Request, res: Response) => {
   });
 };
 
-const loginUser = async (req: Request, res: Response) => {
+const loginUser = async (req: Request, res: Response, next:NextFunction) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    return new CustomError("Invalid Credentials", 400);
+    return next(new CustomError("User not found", 404))
   }
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
-    return new CustomError("Invalid Credentials", 400);
+    return next(new CustomError("User not found", 404))
   }
   const token = jwt.sign(
     { userId: user._id },
